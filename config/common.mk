kn8wolf/vendor_slim

Add new Squisher/Opticharger for Lollipop (2/2)
notice:
-------

needs pngquant (best tool) install on linux system for this feature to work

p4:
   use ascii color codes instead of tput. Its more universal

Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
Change-Id: Ib89addf8cba8a7a48e94df3d9e077b1526d9829c

@fusionjack:
* Move scripts to tools folder
* Define the SQUISHER_SCRIPT
* Slim-ify
* Colorize opticharger
* Force only using pngquant

Change-Id: If877d6f27790e49d7476702c0a7af9ce736741b6

config/common.mk
@@ -205,4 +205,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 EXTENDED_POST_PROCESS_PROPS := vendor/slim/tools/slim_process_props.py
 FINISHER_SCRIPT := vendor/slim/tools/finisher
+SQUISHER_SCRIPT := vendor/slim/tools/squisher
 

SOKP/android_vendor_sokp

config/common.mk
@@ -188,7 +188,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=$(SOKP_MOD_VERSION) \
     ro.sokp.buildtype=$(SOKP_BUILD_TYPE)
 
+# SOKP Tools
 EXTENDED_POST_PROCESS_PROPS := vendor/sokp/tools/sokp_process_props.py
+SQUISHER_SCRIPT := vendor/sokp/tools/squisher
 
 # statistics identity
   PRODUCT_PROPERTY_OVERRIDES += \