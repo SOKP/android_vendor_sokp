kn8wolf/vendor_slim

Finisher script (2/2)
Change-Id: I20363713a827629294ffe71f92ebd2d0001026e3

config/common.mk
@@ -204,4 +204,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.slim.buildtype=$(SLIM_BUILD_TYPE)
 
 EXTENDED_POST_PROCESS_PROPS := vendor/slim/tools/slim_process_props.py
+FINISHER_SCRIPT := vendor/slim/tools/finisher
 

SOKP/android_vendor_sokp

config/common.mk
@@ -190,6 +190,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SOKP Tools
 EXTENDED_POST_PROCESS_PROPS := vendor/sokp/tools/sokp_process_props.py
+FINISHER_SCRIPT := vendor/sokp/tools/finisher
 SQUISHER_SCRIPT := vendor/sokp/tools/squisher
 
 # statistics identity