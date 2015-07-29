c0982cab6444fb25ca02ddc65a394402185d1c27
AudioGod <AudioGod@Sonic-Developers.com>
2015-07-30 03:35:30 +0530

    Add UNIVERSAL Prebuilt QuickBoot apk Credits @arter97
    [UNIVERSAL] QuickBoot for CyanogenMod 12.0 / 12.1 [Other ROMS ALSO]
    
    /* Info */
    
    QuickBoot is a Cyanogen OS feature developed by Qualcomm and Cyanogen for OnePlue One.
    The goal is to reduce the boot-up time drastically by altering the shutdown / boot-up sequence.
    
    See QuickBoot in action :
    https://www.youtube.com/watch?v=sWAvPwZDXKM
    https://www.youtube.com/watch?v=aHaZSY_yad4
    
    More Details:http://forum.xda-developers.com/android/software/universal-quickboot-cyanogenmod-12-0-12-t3082041
    
    Signed-off-by: AudioGod <AudioGod@Sonic-Developers.com>

--- config/common.mk
+++ config/common.mk
@@ -124,6 +124,7 @@ vendor/sokp/proprietary/fonts/Roboto-LightItalic.ttf:system/fonts/Roboto-LightIt
 vendor/sokp/proprietary/fonts/Roboto-Regular.ttf:system/fonts/Roboto-Regular.ttf \
 vendor/sokp/proprietary/app/FileManager/MiXplorer_v5.2.18_build_201504182_test.apk:system/app/FileManager/MiXplorer_v5.2.18_build_201504182_test.apk \
 vendor/sokp/proprietary/app/SOKPStats/SOKPStats.apk:system/app/SOKPStats/SOKPStats.apk \
+vendor/sokp/proprietary/priv-app/QuickBoot/QuickBoot.apk:system/priv-app/QuickBoot/QuickBoot.apk \
 vendor/sokp/proprietary/app/LayersManager/com.lovejoy777.rroandlayersmanager-12.apk:system/app/LayersManager/com.lovejoy777.rroandlayersmanager-12.apk
 
 # CM Hardware Abstraction Framework
